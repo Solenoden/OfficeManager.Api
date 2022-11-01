1c11f31abd8453f814d88abd3cff3eff3cb6cfdc
Gavin de Bruyn <gavindb@runninghill.co.za>
2022-11-01 08:40:12 +0200

    Create logic to connect to the database and initialize the tables
    Signed-off-by: Gavin de Bruyn <gavindb@runninghill.co.za>

---
+++ enums/database-table.enum.ts
@@ -0,0 +1,4 @@
+export enum DatabaseTable {
+    Office = 'office',
+    OfficeWorker = 'officeWorker'
+}
---
+++ errors/app-errors.ts
@@ -0,0 +1,28 @@
+export class HttpError implements Error {
+    name: string
+    message: string
+    statusCode: number
+    originalError: any
+    origin: string
+
+    constructor(message: string, statusCode: number, originalError: any, origin: string) {
+        this.name = this.constructor.name
+        this.message = message
+        this.statusCode = statusCode
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        this.originalError = originalError
+        this.origin = origin
+    }
+}
+
+export class UnexpectedError extends HttpError {
+    constructor(public originalError: any, origin: string) {
+        super('An unexpected error occurred. Please try again later.', 500, originalError, origin)
+    }
+}
+
+export class DatabaseError extends HttpError {
+    constructor(public originalError: any, origin: string) {
+        super('An unexpected database error occurred.', 500, originalError, origin)
+    }
+}
